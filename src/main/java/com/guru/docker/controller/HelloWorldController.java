13b454a00336c0fd06fb8c9adb30a55c96d416b9
gpnguru <guruprasanna.nagaraju@newage-global.com>
2024-09-26 11:55:41 +0530

    Initial commit

---
+++ src/main/java/com/guru/docker/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.guru.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloworld(){
+        return "Hello world";
+    }
+}
---
+++ src/test/java/com/guru/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
